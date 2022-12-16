1f2bed9f3ac2beab807741e486375a5574def594
Hazim Azmie <hazim.azmie@fdmgroup.com>
2022-12-16 19:14:52 +0800

    Algorithm 1 solutions

--- src/main/java/com/hazim/code/algorithm1/LeetCode.java
+++ src/main/java/com/hazim/code/algorithm1/LeetCode.java
@@ -2,9 +2,82 @@ package com.hazim.code.algorithm1;
 
 public class LeetCode {
     public static void main(String[] args) {
-        int[] nums = {1,3,5,6};
-        int t = 2;
-        System.out.println(searchIndex(nums,t));
+        int[] arr = {0,1,0,3,12};
+        moveZeros(arr);
+        for(int i : arr)
+            System.out.println(i);
+    }
+
+    public static int[] twoSum(int[] nums,int target){
+        if(nums.length<2){
+            return nums;
+        }
+        int start = 0;
+        int end = nums.length-1;
+        int[] ans = new int[2];
+        while(start<end){
+            int sum = nums[start] + nums[end];
+            if(sum == target){
+                ans[0] = start+1;
+                ans[1] = end+1;
+                break;
+            }
+            if(sum < target){
+                ++start;
+            }else{
+                --end;
+            }
+        }
+        return ans;
+    }
+    public static void moveZeros(int[] nums){
+        int idx = 0;
+        for(int i : nums){
+            if(i!=0){
+                nums[idx++] = i;
+            }
+        }
+        for(int i = idx ; i < nums.length; ++i){
+            nums[i] = 0;
+        }
+    }
+    public static int[] rotateArrays(int[] nums, int k){
+        //dont have to do exactly k-steps, just the remainder is sufficient
+        k%=nums.length;
+        reverse(nums,0,nums.length-1);
+        reverse(nums,0,k-1);
+        reverse(nums,k,nums.length-1);
+
+        return nums;
+    }
+    public static void reverse(int[] nums, int start, int end){
+        while(start<end){
+            int temp = nums[start];
+            nums[start] = end;
+            nums[end] = temp;
+            ++start;
+            --end;
+        }
+    }
+
+    public static int[] sortedSquares(int[] nums){
+        int start = 0;
+        int end = nums.length-1;
+        int idx = end;
+        int[] ans = new int[nums.length];
+        while(start<=end){
+            int startSq = nums[start]*nums[start];
+            int endSq = nums[end]*nums[end];
+            if(startSq>endSq){
+                ans[idx--] = startSq;
+                ++start;
+            }
+            else{
+                ans[idx--] = endSq;
+                --end;
+            }
+        }
+        return ans;
     }
     public static int search(int[] nums, int target){
         int left = 0;
